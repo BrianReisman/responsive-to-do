c1b163652b687f62ef0253d04b278c0735b59b94
BrianReisman <brianreisman@protonmail.com>
2021-04-30 19:17:34 -0400

    add background image with linear-gradient

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import {Root} from './GlobalElements';
+import { Root, BackgroundImg } from "./GlobalElements";
+import img from "./assets/images/bg__trees.png";
 
 function App() {
   return (
     <Root>
-
+      <BackgroundImg src={img} alt="forest landscape background" />
     </Root>
   );
 }
--- src/GlobalElements.js
+++ src/GlobalElements.js
@@ -1,6 +1,21 @@
 import styled from "styled-components";
+import img from "./assets/images/bg__trees.png";
 
 export const Root = styled.div`
   background-color: #181824;
   height: 100vh;
+`;
+
+export const BackgroundImg = styled.div`
+  z-index: -10;
+  background-image: linear-gradient(
+      131.73deg,
+      rgba(114, 190, 248, 0.8) 14.46%,
+      rgba(166, 116, 242, 0.8) 87.62%
+    ),
+    url(${img});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 45vh;
 `;
